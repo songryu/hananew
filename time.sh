538b6d98e8419655efcc919d6321bea28fc91c45
songryu <thdfb2941@naver.com>
2024-06-12 05:26:01 +0000

    git add ! 현재시간은: 2024-06-12 05:26:01

---
+++ time.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+time=$(date "+%Y-%m-%d %H:%M:%S")
+echo $time
+echo $time >> /home/ubuntu/homework/zz.txt
+
+cd /home/ubuntu/homework/hananew
+
+git add .
+git commit -m "git add ! 현재시간은: $time"
+echo "commit complete!"
+git push origin master
+echo "push complete!"
